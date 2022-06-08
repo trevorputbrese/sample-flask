d05e2c91365fd5d0625d779fc8bfe34974e5cd23
Trevor Putbrese <trevor.putbrese@gmail.com>
2022-06-08 14:50:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask_pyctuator.flask_pyctuator import FlaskPyctuator
+
 
 app = Flask(__name__)
 
@@ -7,3 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+FlaskPyctuator(
+    app,
+)
+
+app.run(debug=False, port=8081, host="0.0.0.0")
